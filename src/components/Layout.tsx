469e67fc356346de55bd755f4e17c6733841bdac
Reza Azarnia <rezaazarnia158@gmail.com>
2025-02-02 00:04:49 +0330

    fix : make website invisible in window size

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router";
 
 export default function Layout() {
     return (
-        <div className='flex flex-col min-h-screen text-white bg-black'>
+        <div className='flex flex-col min-h-screen text-white'>
             <Outlet />
         </div>
     )
